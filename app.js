fd1930accce0d20017c8e444fe0a95b5c887d891
krowker <krowker97@gmail.com>
2021-09-23 14:48:01 +0300

    create

---
+++ app.js
@@ -0,0 +1,11 @@
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+   slide.addEventListener('click', () => {
+      slides.forEach((slide)=>{
+         slide.classList.remove('active')
+      })
+      
+      slide.classList.add('active')
+   })
+}
